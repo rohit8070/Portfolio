4aadf221454ce475d1d2b137d9ec074ac36e693f
Rohit mishra <rohit80709@gmail.com>
2024-09-05 16:15:15 +0530

    add projects

--- src/components/Projects/projects.jsx
+++ src/components/Projects/projects.jsx
@@ -38,40 +38,48 @@ export default function Projects() {
                   onClick={() => toggleTab(0)}
                   className="uil uil-times projects_modal-close"
                 ></i>
-                <h3 className="projects_modal-title">Project 1</h3>
+                <h3 className="projects_modal-title">React Portfolio</h3>
                 <p className="projects_modal-description">
-                  projects with more than 3 years of experience. Providing
-                  quality work tko clients and companies.
+                  A portfolio project showcasing my skills in front-end
+                  development using React. Providing seamless and responsive
+                  user interfaces with modern tools and technologies.
                 </p>
 
                 <ul className="prjects_modal-projects grid">
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      I develop the user interface
+                      Built with React and JavaScript.
                     </p>
                   </li>
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
-                    <p className="projects_modal-info">webpage development </p>
+                    <p className="projects_modal-info">
+                      Responsive design with CSS and styled-components.
+                    </p>
                   </li>
 
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      I create uix element interactions
+                      Dynamic UI/UX interactions with React hooks.{" "}
                     </p>
                   </li>
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      I position your company brand{" "}
+                      Optimized for performance and user experience.
                     </p>
                   </li>
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      Design and mockups of products for comppnies
+                      <a
+                        href="https://rohit8070.github.io/Portfolio/"
+                        target="_blank"
+                      >
+                        click to visit
+                      </a>
                     </p>
                   </li>
                 </ul>
@@ -82,7 +90,7 @@ export default function Projects() {
           <div className="project_content">
             <div>
               <i className="uil uil-arrow projects_icon"></i>
-              <h3 className=" project_title">Airbnb Clone</h3>
+              <h3 className=" project_title">CRUD Operations</h3>
             </div>
             <span className="projects_button" onClick={() => toggleTab(2)}>
               View More
@@ -104,40 +112,49 @@ export default function Projects() {
                   onClick={() => toggleTab(0)}
                   className="uil uil-times projects_modal-close"
                 ></i>
-                <h3 className="projects_modal-title">Project 2</h3>
+                <h3 className="projects_modal-title">
+                  CRUD Operations with JDBC
+                </h3>
                 <p className="projects_modal-description">
-                  projects with more than 3 years of experience. Providing
-                  quality work tko clients and companies.
+                  A backend project demonstrating Create, Read, Update, and
+                  Delete (CRUD) operations using Java and JDBC.{" "}
                 </p>
 
                 <ul className="prjects_modal-projects grid">
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      I develop the user interface
+                      Developed using Java with JDBC for database interaction.
                     </p>
                   </li>
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
-                    <p className="projects_modal-info">webpage development </p>
+                    <p className="projects_modal-info">
+                      Implements essential CRUD functionality.
+                    </p>
                   </li>
 
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      I create uix element interactions
+                      Demonstrates database connection and manipulation.
                     </p>
                   </li>
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      I position your company brand{" "}
+                      Manages database records efficiently.
                     </p>
                   </li>
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      Design and mockups of products for comppnies
+                      <a
+                        href="https://github.com/rohit8070/JDBC-CRUD-operations.git"
+                        target="_blank"
+                      >
+                        Click to get sourcecode
+                      </a>
                     </p>
                   </li>
                 </ul>
@@ -148,7 +165,7 @@ export default function Projects() {
           <div className="project_content">
             <div>
               <i className="uil uil-edit projects_icon"></i>
-              <h3 className=" project_title">Airbnb Clone</h3>
+              <h3 className=" project_title">Mini PROJECTS</h3>
             </div>
             <span className="projects_button" onClick={() => toggleTab(3)}>
               View More
@@ -170,40 +187,38 @@ export default function Projects() {
                   onClick={() => toggleTab(0)}
                   className="uil uil-times projects_modal-close"
                 ></i>
-                <h3 className="projects_modal-title">Project 3</h3>
+                <h3 className="projects_modal-title">
+                  Tic-Tac-Toe & Simon Says
+                </h3>
                 <p className="projects_modal-description">
-                  projects with more than 3 years of experience. Providing
-                  quality work tko clients and companies.
+                  Explore two engaging mini-projects built with html css and Js:
+                  a classic Tic-Tac-Toe game and a Simon Says game. Both
+                  projects focus on interactive user experiences and responsive
+                  designs.
                 </p>
 
                 <ul className="prjects_modal-projects grid">
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
-                    <p className="projects_modal-info">
-                      I develop the user interface
-                    </p>
-                  </li>
-                  <li className="projects_modal-project">
-                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                    <p className="projects_modal-info">webpage development </p>
+                    <p className="projects_modal-info">Tic-Tac-Toe Game:</p>
                   </li>
 
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      I create uix element interactions
+                      <a href="">Click to play Tic Tac Toe</a>
                     </p>
                   </li>
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
-                    <p className="projects_modal-info">
-                      I position your company brand{" "}
-                    </p>
+                    <p className="projects_modal-info">Simon Says Game:</p>
                   </li>
                   <li className="projects_modal-project">
                     <i className="uil uil-check-circle projects_modal-icon"></i>
                     <p className="projects_modal-info">
-                      Design and mockups of products for comppnies
+                      <a href="https://rohit8070.github.io/simanSaysGame/">
+                        Click to play simon says game
+                      </a>
                     </p>
                   </li>
                 </ul>
